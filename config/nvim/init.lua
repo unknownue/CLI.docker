348b44f9a130fed798d6e109a0a455de2b7fac69
unknownue <unknownue@outlook.com>
2022-08-02 10:22:15 +0800

    Update neovim path for Windows

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -14,24 +14,43 @@
 -- To see how to translate *.vim to *.lua, see https://vonheikemen.github.io/devlog/tools/configuring-neovim-using-lua/
 -- Use 'source config/nvim/init.lua' to reload all config
 
--- Add absolute search path
-package.path = package.path .. ";/Users/sylveon/Dev/dotfiles/config/nvim/?.lua;"
--- print(package.path)
 
-if not vim.g.vscode then
-    vim.env.XDG_CONFIG_HOME = "/Users/sylveon/Dev/dotfiles/config"
+if vim.loop.os_uname().sysname == "Windows_NT" then
+    package.path = package.path .. "C:\\Users\\%USERNAME%\\AppData\\Local\\nvim\\?.lua;C:\\Users\\%USERNAME%\\AppData\\Local\\nvim\\lua\\?.lua;"
+    if not vim.g.vscode then
+        vim.env.XDG_CONFIG_HOME = "C:\\Users\\%USERNAME%\\AppData\\Local"
+    end
+
+    -- Basic configration
+    require('config')
+    -- Appearence settings (only work in terminal)
+    require('appearence')
+    -- Customized functions
+    require('function')
+    -- Load plugins
+    require('plugs')
+    -- Keyboard settings
+    require('colemak')
+else
+    -- Add absolute search path
+    package.path = package.path .. ";/Users/sylveon/Dev/dotfiles/config/nvim/?.lua;"
+    -- print(package.path)
+
+    if not vim.g.vscode then
+        vim.env.XDG_CONFIG_HOME = "/Users/sylveon/Dev/dotfiles/config"
+    end
+
+    -- Basic configration
+    require('lua.config')
+    -- Appearence settings (only work in terminal)
+    require('lua.appearence')
+    -- Customized functions
+    require('lua.function')
+    -- Load plugins
+    require('lua.plugs')
+    -- Keyboard settings
+    require('lua.colemak')
 end
-
--- Basic configration
-require('lua.config')
--- Appearence settings (only work in terminal)
-require('lua.appearence')
--- Customized functions
-require('lua.function')
--- Load plugins
-require('lua.plugs')
--- Keyboard settings
-require('lua.colemak')
 
 print('Configuration load succeed...')
 
--- config/nvim/lua/plugs.lua
+++ config/nvim/lua/plugs.lua
@@ -12,9 +12,22 @@
 -- -------------------------------------------------------------------------
 
 
-local plugin_path = vim.env.XDG_CONFIG_HOME .. '/nvim/autoload/plug.vim'
+local plugin_path
+if vim.loop.os_uname().sysname == "Windows_NT" then
+    plugin_path = vim.env.XDG_CONFIG_HOME .. 'nvim-data\\site\\autoload\\plug.vim'
+else
+    plugin_path = vim.env.XDG_CONFIG_HOME .. '/nvim/autoload/plug.vim'
+end
+
+
 if not file_exists(plugin_path) then
-    local download_cmd = 'curl -fLo ' .. plugin_path .. ' --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+    local download_cmd
+    if vim.loop.os_uname().sysname == "Windows_NT" then
+        download_cmd = 'curl -fLo ' .. plugin_path .. ' --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+    else
+        download_cmd = 'iwr -useb https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim | ni "$(@($env:XDG_DATA_HOME, $env:LOCALAPPDATA)[$null -eq $env:XDG_DATA_HOME])/nvim-data/site/autoload/plug.vim" -Force'
+    end
+    
     os.execute(download_cmd)
     print('Download plug.vim from vim-plug. May be "PlugInstall/PlugUpdate is needed."')
 end
@@ -22,7 +35,7 @@ end
 
 local Plug = vim.fn['plug#']
 
-vim.call('plug#begin', '$XDG_CONFIG_HOME/nvim/plugs')
+vim.call('plug#begin')
 
 Plug 'AndrewRadev/sideways.vim'  -- switch function parameters
 Plug 'AndrewRadev/splitjoin.vim' -- switching statement between single-line and multi-lines
